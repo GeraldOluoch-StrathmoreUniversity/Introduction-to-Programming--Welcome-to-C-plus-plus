4c6c0c5403b4138d525c7bc5bd19e0371c5f437c
PC <gerald.oluoch@strathmore.edu>
2020-05-22 18:32:42 +0300

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/Welcome_To_C__.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/gerald/Documents/SRCs/2.SRCs-Dojo-University/Local/Bachelors/Strathmore University/7. Retakes/1. Introduction to Programming/Assignments/Welcome To C++/main.cpp" "/Users/gerald/Documents/SRCs/2.SRCs-Dojo-University/Local/Bachelors/Strathmore University/7. Retakes/1. Introduction to Programming/Assignments/Welcome To C++/cmake-build-debug/CMakeFiles/Welcome_To_C__.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Welcome_To_C__.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Welcome_To_C__.dir/main.cpp.o"
+  "Welcome_To_C__"
+  "Welcome_To_C__.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Welcome_To_C__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
